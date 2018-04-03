870940c931cce76a63f262e00b8276aba8609a5a
Shea Yonker <sbyonker@gmail.com>
2018-04-02 21:43:01 -0700

    Update RedeuceBounce.cs

--- CSE165Project4/Assets/Scripts/RedeuceBounce.cs
+++ CSE165Project4/Assets/Scripts/RedeuceBounce.cs
@@ -7,7 +7,6 @@ public class RedeuceBounce : MonoBehaviour {
     // Update is called once per frame
     void FixedUpdate()
     {
-        //Vector3 temp = new Vector3(GetComponent<Rigidbody>().velocity.x, 0.0f , GetComponent<Rigidbody>().velocity.z);
         GetComponent<Rigidbody>().velocity = Vector3.zero;
     }
 }
